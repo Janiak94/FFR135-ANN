74cd5dab1381701a64be6f0810a879f7e68c364e
Jens Nilsson <jenszza@gmail.com>
2021-09-13 10:57:39 +0200

    Added third task

--- Homework1/hopfield.py
+++ Homework1/hopfield.py
@@ -46,6 +46,7 @@ class Hopfield:
             self.pattern_size = patterns.pattern_length
         else:
             raise TypeError("Uknown type of patterns: {}".format(patterns))
+        self.order_param = None
         self.zero_diag = zero_diag
         self.stored_patterns = patterns
         self.set_state(initial_state)
@@ -68,21 +69,39 @@ class Hopfield:
         if self.zero_diag:
             np.fill_diagonal(self.weights, 0)
 
-    def update(self, n_iter, update_scheme="sync", initial_state=None):
+    def update(self, n_iter, update_scheme="async", initial_state=None, noise=None, calc_order_param=False):
         if initial_state is not None: # If we want to feed in another pattern
             self.set_state(initial_state)
+        else:
+            initial_state = self.state
+        order_param = 0.0
         has_changed = False
         if update_scheme == "async" or update_scheme == "async_typewriter":
             # Perform async updates
             for i in range(n_iter):
+                # Choose neuron to update
                 if update_scheme == "async":
                     neuron = np.random.randint(self.pattern_size) # Choose random neuron to update
-                else:
+                elif update_scheme == "async_typewriter":
                     neuron = i % self.pattern_size # Use typewriter scheme (pattern is flattened so this works)
                 old_val = self.state.pattern[neuron]
-                self.state.pattern[neuron] = Hopfield._sgn(self.weights[neuron,:] @ self.state.pattern) # Update single neuron state
-                if has_changed is False and self.state.pattern[neuron] != old_val: # Check if neuron was flipped
+                # Update value
+                if noise is not None:
+                    local_field = self.weights[neuron,:] @ self.state.pattern
+                    p = 1 / (1 + np.exp(-2*local_field*noise))
+                    # Stochastic update of neuron
+                    if np.random.rand() <= p:
+                        self.state.pattern[neuron] = 1
+                    else:
+                        self.state.pattern[neuron] = -1
+                else:
+                    self.state.pattern[neuron] = Hopfield._sgn(self.weights[neuron,:] @ self.state.pattern) # Update single neuron state
+                if calc_order_param:
+                    order_param += self.state.pattern @ initial_state.pattern # s_i * x_i
+                # Check if neuron was flipped
+                if has_changed is False and self.state.pattern[neuron] != old_val:
                     has_changed = True
+        self.order_param = order_param / (n_iter * self.pattern_size)
         return has_changed
 
 def print_pattern(pattern):
@@ -98,10 +117,9 @@ def print_pattern(pattern):
             print(p_image[i,-1], end="]")
     print("]")
 
-
+# First task
 def one_step_error(zero_diag):
-    # First task
-    N_TRIALS = 100
+    N_TRIALS = 100000
     N = 120 # Size of patterns
     N_UPDATES = 1 # Number of updates for the network in each trial
     p_vals = [12,24,48,70,100,120] # Number of patterns in each trial
@@ -120,6 +138,7 @@ def one_step_error(zero_diag):
         one_step_error_prob.append(n_errors)
     print("One-step error probability: [{:.4f}, {:.4f}, {:.4f}, {:.4f}, {:.4f}, {:.4f}]".format(*one_step_error_prob))
 
+# Second task
 def recognising_digits(pattern_number):
     digits = [Pattern(x) for x in [pt.x1, pt.x2, pt.x3, pt.x4, pt.x5]]
     N_UPDATES = 100 * digits[0].pattern_length * 1
@@ -192,24 +211,38 @@ def recognising_digits(pattern_number):
     plt.matshow(network.state.get_image(), cmap=bw_cmap)
     plt.show()
 
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    subparsers = parser.add_subparsers(help="Task number", dest="task_number")
-
-    first_parser = subparsers.add_parser("1", help="The first task")
-    first_parser.add_argument("-z", help="Flag if diagonals in the weight matrix in the first task should be zero", action="store_true", dest="zero_diag")
-
-    second_parser = subparsers.add_parser("2", help="The second task")
-    second_parser.add_argument("pattern_number", help="Which pattern to use", choices=["1","2","3"])
-
-    # Add other parsers
-    args = parser.parse_args()
-    if args.task_number == "1":
-        one_step_error(args.zero_diag)
-    if args.task_number == "2":
-        recognising_digits(args.pattern_number)
+# Third task
+def stochastic_hopfield_network(n_patterns):
+    PATTERN_SIZE = 200
+    BETA = 2
+    N_UPDATES = 2*100000
+    N_TRIALS = 100
+    order_param_list = []
+    for i in range(N_TRIALS):
+        patterns = generate_patterns(PATTERN_SIZE, n_patterns) # n_patterns patterns with PATTER_SIZE bits
+        network = Hopfield(patterns, zero_diag=True) # Store patterns in network
+        network.update(N_UPDATES, update_scheme="async", initial_state=patterns[0], noise=BETA, calc_order_param=True) # Perform updates
+        order_param_list.append(network.order_param)
+    print("{:.3f}".format(np.mean(order_param_list))) # Print the mean of the order parameters attained
 
 
+parser = argparse.ArgumentParser()
+subparsers = parser.add_subparsers(help="Task number", dest="task_number")
 
+first_parser = subparsers.add_parser("1", help="The first task")
+first_parser.add_argument("-z", help="Flag if diagonals in the weight matrix in the first task should be zero", action="store_true", dest="zero_diag")
 
+second_parser = subparsers.add_parser("2", help="The second task")
+second_parser.add_argument("pattern_number", help="Which pattern to use", choices=["1","2","3"])
+
+third_parser = subparsers.add_parser("3", help="The third task")
+third_parser.add_argument("number_of_patterns", help="The number of random patterns to use", type=int)
+
+# Add other parsers
+args = parser.parse_args()
+if args.task_number == "1":
+    one_step_error(args.zero_diag)
+if args.task_number == "2":
+    recognising_digits(args.pattern_number)
+if args.task_number == "3":
+    stochastic_hopfield_network(args.number_of_patterns)
