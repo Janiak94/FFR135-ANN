8b8737fe664f5e81f2f43137260b6eff3db5420f
Jens Nilsson <jenszza@gmail.com>
2021-09-21 22:53:27 +0200

    Implemented batch training

--- Homework2/OneLayerPerceptron.py
+++ Homework2/OneLayerPerceptron.py
@@ -1,9 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def normalize_data(data):
-    return (data-np.mean(data)) / np.std(data)
-
 def get_random_array(shape):
     """
     returns array with entries
@@ -12,6 +9,20 @@ def get_random_array(shape):
     """
     w = 2 * np.random.rand(shape[0], shape[1]) - 1
     return w
+
+def batch_data(training_data, batch_size):
+    """
+    Form random batches from the training data
+    and return them as a list, each entry containing a batch
+    """
+    n_data_points = training_data.shape[0]
+    shuffled_data = training_data.copy() # Make copy so we do not touch the original
+    np.random.shuffle(shuffled_data) # Shuffle the data to get different batches in each run
+    batches = []
+    for i in range(0, n_data_points, batch_size): # Form batches
+        batch = shuffled_data[i:(i+batch_size),:]
+        batches.append(batch)
+    return batches
 
 def get_network_parameters(layer_sizes):
     """
@@ -37,95 +48,128 @@ def feed_forward(input_data, weights, tresholds):
         state = np.tanh(local_field)
     return np.sign(np.squeeze(state))
 
-def stochastic_gradient_descent(training_data, weights, tresholds, n_iter, learning_rate):
-    training_inputs = training_data[:,0:2]
-    training_labels = training_data[:,-1]
+def stochastic_gradient_descent(training_data, weights, tresholds, n_epoch, learning_rate, batch_size):
     activation_function = np.tanh
     activation_derivative = (lambda x: (1 / np.cosh(x))**2)
     depth = len(weights) # Number of hidden layers plus output layer
-    for i in range(n_iter):
-        r = np.random.randint(training_inputs.shape[0]) # Chose random pattern index
-        state = training_inputs[r,:].reshape((-1,1)) # Set initial state (input neurons)
-        local_fields = []
-        states = [state]
-        errors = []
-        for i in range(depth): # Perform feed forward
-            local_field = weights[i] @ state - tresholds[i]
-            state = np.tanh(local_field) # Update state
-            local_fields.append(local_field) # Save values for backpropagation later
-            states.append(state) # Save values for when updating weights
-        output = state
-        output_error = activation_derivative(local_fields[-1]) * (training_labels[r] - output)
-        errors = [output_error]
-        for i in range(depth-1, 0, -1): # Perform backpropagation
-            t1 = (errors[0] @ weights[i]).T
-            t2 = activation_derivative(local_fields[i-1])
-            err = np.multiply(t1, t2)
-            errors.insert(0, err)
-        for i in range(depth): # Update parameters
-            weights[i] = weights[i] + learning_rate * np.outer(errors[i], states[i])
-            tresholds[i] = tresholds[i] - learning_rate * errors[i]
+    for i in range(n_epoch):
+        print("Epoch {}/{}".format(i, n_epoch))
+        batches = batch_data(training_data, batch_size) # Generate batches of data
+
+        # Perform batch training
+        for batch in batches:
+            batch_pattern = batch[:,0:2].T # Separate the patterns from labels and make column vectors
+            batch_labels = batch[:,-1]
+            state = batch_pattern # Set initial state
+            local_fields = [] # Store locals fields for each layer and pattern
+            states = [state] # Store neuron states for each layer and pattern
+            errors = [] # Store error terms for each layer and pattern
+
+            # Perform feed forward
+            for j in range(depth):
+                local_field = weights[j] @ state - tresholds[j] # Should return array with shape == (x, batch_pattern.shape[1])
+                state = activation_function(local_field) # Shouldnt change the shape
+                local_fields.append(local_field) # Save field for later
+                states.append(state) # Save state for later
+            output = state
+            output_error = activation_derivative(local_fields[-1]) * (batch_labels - output) # Calculate the output errors for each pattern
+
+            errors = [output_error]
+            # Perform backpropagation
+            for j in range(depth-1, 0, -1):
+                t1 = errors[0].T @ weights[j]
+                t2 = activation_derivative(local_fields[j-1])
+                err = t1.T * t2
+                errors.insert(0, err)
+
+            # Update weights
+            for j in range(depth):
+                dw = np.zeros(weights[j].shape)
+                dt = np.zeros(tresholds[j].shape).T
+                # Add errors for each pattern (use np sum)
+                for k in range(batch_pattern.shape[1]):
+                    dw += np.outer(errors[j][:,k], states[j][:,k])
+                    dt += errors[j][:,k]
+                weights[j] += learning_rate * dw
+                tresholds[j] -= learning_rate * dt.T
     return (weights, tresholds)
 
+if __name__ == "__main__":
+    ############################
+    # Import data sets
+    ############################
+    TRAINING_DATA_PATH = "training_data/training_set.csv"
+    VALIDATION_DATA_PATH = "training_data/validation_set.csv"
 
-############################
-# Import data sets
-############################
-TRAINING_DATA_PATH = "training_data/training_set.csv"
-VALIDATION_DATA_PATH = "training_data/validation_set.csv"
+    training_data = np.genfromtxt(TRAINING_DATA_PATH, delimiter=",")
+    validation_data = np.genfromtxt(VALIDATION_DATA_PATH, delimiter=",")
 
-training_data = np.genfromtxt(TRAINING_DATA_PATH, delimiter=",")
-validation_data = np.genfromtxt(VALIDATION_DATA_PATH, delimiter=",")
+    ############################
+    # Normalize data
+    ############################
+    mean = np.mean(training_data[:,0:2])
+    std = np.std(training_data[:,0:2])
+    training_data[:,0:2] = (training_data[:,0:2] - mean) / std
+    validation_data[:,0:2] = (validation_data[:,0:2] - mean) / std
 
-training_data = 2*np.random.rand(10000, 3)-1
-for i in range(10000):
-    training_data[i,2] = 1 if (training_data[i,0]**2 + training_data[i,1]**2 < 0.5) else 0
+    #############################
+    # Number of neurons in each layer
+    #############################
+    M1 = 10 # Number of hidden neurons
+    input_size = training_data.shape[1] - 1 # Subtract 1 for the labels
+    output_size = 1
+    layer_sizes = [input_size, M1, output_size]
 
+    #############################
+    # Initialize weights and treshold
+    #############################
+    (weights, tresholds) = get_network_parameters(layer_sizes)
 
-############################
-# Normalize data
-############################
-validation_data[:,0:2] = normalize_data(validation_data[:,0:2])
+    #############################
+    # Run stochastic gradient descent
+    #############################
+    n_epoch = 5000
+    learning_rate = 5e-4
+    batch_size = 50
+    (weights, tresholds) = stochastic_gradient_descent(
+        training_data,
+        weights, tresholds,
+        n_epoch=n_epoch,
+        learning_rate=learning_rate,
+        batch_size=batch_size
+        )
 
-#############################
-# Number of neurons in each layer
-#############################
-M1 = 30 # Number of hidden neurons
-input_size = training_data.shape[1] - 1 # Subtract 1 for the labels
-output_size = 1
-layer_sizes = [input_size, M1, output_size]
+    #############################
+    # Predict
+    #############################
+    pred = []
+    for i in range(training_data.shape[0]):
+        output = feed_forward(training_data[i,0:2], weights, tresholds)
+        pred.append(output)
 
-#############################
-# Initialize weights and treshold
-#############################
-(weights, tresholds) = get_network_parameters(layer_sizes)
+    #############################
+    # Classification error on validation data set
+    #############################
+    c_error = 0
+    for i in range(validation_data.shape[0]):
+        output = feed_forward(validation_data[i,0:2], weights, tresholds)
+        c_error += np.abs(output - validation_data[i,2])
+    c_error /= 2 * validation_data.shape[0]
+    print("Classification error on validation data set: {:.4f}".format(c_error))
 
-pred2 = []
-for i in range(training_data.shape[0]):
-    output = feed_forward(training_data[i,0:2], weights, tresholds)
-    pred2.append(output)
-print(weights)
-print(tresholds)
-#############################
-# Run stochastic gradient descent
-#############################
-(weights, tresholds) = stochastic_gradient_descent(training_data, weights, tresholds, 1000000, 0.0001)
-print(weights)
-print(tresholds)
-#############################
-# Predict
-#############################
-pred1 = []
-for i in range(training_data.shape[0]):
-    output = feed_forward(training_data[i,0:2], weights, tresholds)
-    pred1.append(output)
+    #############################
+    # Plot training data and predicted data
+    #############################
+    fig, (ax1, ax2) = plt.subplots(1, 2)
+    ax1.scatter(training_data[:,0], training_data[:,1], c=training_data[:,-1], s=1)
+    ax2.scatter(training_data[:,0], training_data[:,1], c=pred, s=1)
+    plt.show()
+    plt.plot()
 
-#############################
-# Plot training data and predicted data
-#############################
-fig, (ax1, ax2, ax3) = plt.subplots(1, 3)
-ax1.scatter(training_data[:,0], training_data[:,1], c=training_data[:,-1], s=1)
-ax2.scatter(training_data[:,0], training_data[:,1], c=pred1, s=1)
-ax3.scatter(training_data[:,0], training_data[:,1], c=pred2, s=1)
-plt.show()
-plt.plot()
+    #############################
+    # Save network paramters
+    #############################
+    np.savetxt("output/w1.csv", weights[0], delimiter=",")
+    np.savetxt("output/w2.csv", weights[1].T, delimiter=",")
+    np.savetxt("output/t1.csv", tresholds[0], delimiter=",")
+    np.savetxt("output/t2.csv", tresholds[1], delimiter=",")
